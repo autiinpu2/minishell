c41748aece1fe6b120c87eae11f69cf6003f1e16
autiinpu2 <aubinytbb@gmail.com>
2026-02-17 02:40:59 +0100

    fix invalid read

--- src/parse/expand.c
+++ src/parse/expand.c
@@ -6,7 +6,7 @@
 /*   By: apuyane <apuyane@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/10 00:46:40 by apuyane           #+#    #+#             */
-/*   Updated: 2026/02/17 00:51:50 by apuyane          ###   ########.fr       */
+/*   Updated: 2026/02/17 02:40:47 by apuyane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ void	expand_variable(char **tab_split, t_data *data, int *j)
 				*j + 1, data);
 		*j = -1;
 	}
-	if (str[*j] == '~')
+	if (str[*j] && str[*j] == '~')
 	{
 		reminder = *j + 1;
 		(*j)++;
