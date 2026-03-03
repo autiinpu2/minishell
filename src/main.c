328b83126fa090c52bc894e91fbe4220f4cff90e
Mathys Comin <mcomin@3A5.42angouleme.fr>
2026-03-03 04:09:22 +0100

    main merged

--- src/main.c
+++ src/main.c
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apuyane <apuyane@student.42angouleme.fr    +#+  +:+       +#+        */
+/*   By: mcomin <mcomin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/25 04:17:27 by apuyane           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2026/02/17 08:13:50 by apuyane          ###   ########.fr       */
-=======
-/*   Updated: 2026/02/19 03:56:31 by apuyane          ###   ########.fr       */
->>>>>>> redir
+/*   Updated: 2026/03/03 03:44:41 by mcomin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +22,8 @@ bool	is_invalid(char *line, t_data *data)
 	{
 		if (check_syntax(line))
 			data->exit_code = 2;
-<<<<<<< HEAD
 		free_single(line);
 		return (true);
-=======
-		return (1);
->>>>>>> redir
 	}
 	return (false);
 }
